5abe7cbc6ccc7077db6029ea5955ef29a8671460
neha-kumari01 <49370243+neha-kumari01@users.noreply.github.com>
2021-07-01 22:41:00 +0530

    Add files via upload

---
+++ angular/interfaceDemo.js
@@ -0,0 +1,9 @@
+;
+var person = {
+    firstName: "Shashwat",
+    lastName: "Tiwari",
+    sayHi: function (firstName, lastName) { return ("Hello " + firstName + " " + lastName); }
+};
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.sayHi("Shash", "wat"));
